aac7975246a47b3cb4266f2facf29079aca726ca
fadfatimah <faldawood0@gmail.com>
2026-04-23 08:34:43 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,7 +2,6 @@ import java.util.Scanner
 
 public class Main {
     public static void main(String[] args) {
-        Scanner input = Sc
-        ;
+         Scanner input = new Scanner(System.in);
     }
 }
